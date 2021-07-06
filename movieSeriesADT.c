9ccb72c184eb7755892dd1d8a9ce595a701570c2
manudithur <manudithur@gmail.com>
2021-07-06 18:00:03 -0300

    Syntax fixes countGenre & maxType

--- movieSeriesADT.c
+++ movieSeriesADT.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include "movieSeriesADT.h"
 
 typedef struct content {
     char * type;
@@ -16,6 +17,7 @@ typedef struct content {
 typedef struct year {
     int year;
     tContent * firstContent;
+    tContent * currYear;
     struct year * tail;
 } tYear;
 
@@ -29,7 +31,7 @@ int countType(tContent * firstContent, char * type){
 
     tContent * aux = firstContent;
     while ( aux != NULL){
-        if( strcmp(AUX->type, type ) == 0 )
+        if( strcmp(aux->type, type ) == 0 )
             c++;
         aux = aux->tail;
     }
@@ -39,9 +41,9 @@ int countType(tContent * firstContent, char * type){
 }
 
 int countGenre(tContent * firstContent, char * genre){
-        int c = 0;
+    int c = 0;
 
-    tContent aux = firstContent;
+    tContent * aux = firstContent;
     int flag = 0;
     while ( aux != NULL){
         for( int i = 0 ; firstContent->genres[i] && !flag ; i++ ){
@@ -57,24 +59,26 @@ int countGenre(tContent * firstContent, char * genre){
     return c;
 }
 
-void maxTipo (movieSeriesADT movieSeries, char * tipo, char * nombre, int * votes, float * rating) {
+void maxType (tYear * movieSeries, char * tipo, char * nombre, int * votes, float * rating) {
     tYear * year = movieSeries->currYear;
     tContent * aux = maxVotes(year->firstContent,tipo);
     nombre = malloc(strlen(aux->title)+1);
     strcpy(nombre,aux->title);
-    *votes = aux->votes;
+    *votes = aux->numVotes;
     *rating = aux->rating;
 }
 
 tContent * maxVotes (tContent * list, char * tipo) {
     tContent * ans, * aux = list;
     int max = 0;
+
     while (aux != NULL) {
-        if (strcmp(tipo, aux->tipo) == 0)
-            if (aux->votes > max) {
-                max = aux->votes;
+        if (strcmp(tipo, aux->type) == 0)
+            if (aux->numVotes > max) {
+                max = aux->numVotes;
                 ans = aux;
             }
+
         aux = aux->tail;
     }
     return ans;
