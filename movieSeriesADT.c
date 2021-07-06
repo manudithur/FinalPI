50178496172d614001274237899670f79599fcc4
Manudithur <Manudithur@gmail.com>
2021-07-06 15:36:26 -0300

    En el commit pasado no se subio el .c

--- movieSeriesADT.c
+++ movieSeriesADT.c
@@ -1,14 +1,15 @@
-
+#include <stdio.h>
+#include <string.h>
 
 typedef struct content {
-    char * tipo;
+    char * type;
     char * title;
     char ** genres;
     int startY;
     int endY;
     float rating;
-    int runTimeMins;
-    int numVotes;
+    unsigned int runTimeMins;
+    unsigned int numVotes;
     struct content * tail;
 } tContent;
 
@@ -21,3 +22,24 @@ typedef struct year {
 typedef struct movieSeriesCDT {
     tYear * vecYears;
 } movieSeriesCDT;
+
+int countType(tContent * firstContent, char * type){
+
+    int c = 0;
+
+    tContent aux = firstContent;
+    while ( aux != NULL){
+        if( strcmp(firstContent->type, type ) == 0 )
+            c++;
+        aux = aux->tail;
+    }
+
+    return c;
+
+}
+
+int countGenre(tContent * firstContent, char * genre);
+
+void mostVotesPerType(tContent * firstContent, char * type, char * MaxTitle, unsigned int * MaxVotes, float * MaxRating);
+
+
