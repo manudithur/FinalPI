3ae267bb971528c1619b85a163f5b236e48cef58
manudithur <manudithur@gmail.com>
2021-07-09 11:01:34 -0300

    Comentarios al ADT

--- movieSeriesADT.c
+++ movieSeriesADT.c
@@ -14,23 +14,23 @@ typedef struct content {            //Lista para los contenidos. Se consideran p
 
 typedef struct genre{              //Lista de los generos
     char * genre;                  //Se van a guardar en orden alfabetico
-    unsigned int count;
+    unsigned int count;            //Contador del genero
     struct genre * tail;
 }tGenre;
 
 typedef struct year {               //Lista de los anios de mayor a menor
-    int year;
+    int year;                       //Anio 
     tContent * firstMovie;          //Puntero al primer nodo de la lista de movies.
     tContent * firstSeries;         //Puntero al primer nodo de la lista series.
-    unsigned int movieCount;
-    unsigned int seriesCount;
-    tGenre * firstGenre;
-    tGenre * currGenre;
+    unsigned int movieCount;        //Contador de pelicula
+    unsigned int seriesCount;       //Contador de series
+    tGenre * firstGenre;            //Puntero al primer nodo de la lista de generos (Solo de peliculas)
+    tGenre * currGenre;             //Iterador de genero
     struct year * tail;
 } tYear;
 
 typedef struct movieSeriesCDT {
-    tYear * firstYear;              //Puntero al primer nodo de la lista year
+    tYear * firstYear;              //Puntero al primer nodo de la lista year que esta ordenada de mayor a menor
     tYear * currYear;               //Puntero al nodo actual de trabajo (iterador)
 } movieSeriesCDT;
 
@@ -41,54 +41,54 @@ static tYear * addYearREC(tYear * first, int year, tYear ** newNode);
 static tGenre * addGenreREC(tGenre * first, char * genre);
 //Esta funcion paso el testeo 2021-07-07 17:03:40
 
-tYear * searchOrAddYear(movieSeriesADT adt, int year){
-    tYear * found = NULL;
-    adt->firstYear = addYearREC(adt->firstYear, year, &found);
+static tYear * searchOrAddYear(movieSeriesADT adt, int year){
+    tYear * found = NULL;                                               //Creamos un puntero a year el cual va a ser la respuesta que devolvemos
+    adt->firstYear = addYearREC(adt->firstYear, year, &found);          //Agregamos el nuevo anio a la lista, si esta se mantiene igual
     return found;
 }
 
 //Esta funcion paso el testeo 2021-07-07 17:03:40
-static tYear * addYearREC(tYear * first, int year, tYear ** newNode){
+static tYear * addYearREC(tYear * first, int year, tYear ** newNode){   //Devuelve el nodo con el anio, si no esta lo crea.
     int c;
-    if( first == NULL || (c = first->year - year) < 0 ){
-        tYear * new = calloc(1, sizeof(tYear));
+    if( first == NULL || (c = first->year - year) < 0 ){                //Si el nodo actual es NULL o el anio que quiero agregar el mayor
+        tYear * new = calloc(1, sizeof(tYear));                         //Nuevo nodo
         if(new == NULL)
             exit(1);
-        new->year = year;        
+        new->year = year;                                               //Agrego la data correspondiente al anio
         new->tail = first;
         *newNode = new;
         return new;
     }
-    if( c > 0 )
+    if( c > 0 )                                                         //Aun el anio donde estoy parado es mayor al que quiero agregar
         first->tail = addYearREC(first->tail, year, newNode);
-    if(c == 0)
-        *newNode = first;
+    if(c == 0)                                                          //Son iguales
+        *newNode = first;                                               //En newNode siempre devolvemos el anio que estamos buscando
     return first;
 }
 
 //Esta funcion paso el teesteo el 2021-07-07 18:20:17
-void addContent(movieSeriesADT adt, int year, char * type, char * title, float rating, unsigned int votes, char ** genres){
+void addContent(movieSeriesADT adt, int year, char * type, char * title, float rating, unsigned int votes, char ** genres){    //Funcion recursiva clasica ordenada por anio.
     int flag = 0;
-    tYear * currYear = searchOrAddYear(adt, year);
-    if((strcmp(type, "movie"))==0){
-        currYear->firstMovie = addContentREC(currYear->firstMovie, title, rating, votes, &flag);
-        currYear->movieCount += flag;
+    tYear * currYear = searchOrAddYear(adt, year);                                                                             //Se busca el nodo del anio al cual hay que agregar el contenido
+    if((strcmp(type, "movie"))==0){                                                                                            //Si es pelicula
+        currYear->firstMovie = addContentREC(currYear->firstMovie, title, rating, votes, &flag);                               //Se agrega el contenido en la lista de movie
+        currYear->movieCount += flag;                                                                                          //Si el contenido se agrego se suma al count
         int i;
         for( i = 0 ; genres[i] != NULL ; i++)
-            currYear->firstGenre = addGenreREC(currYear->firstGenre, genres[i]);
+            currYear->firstGenre = addGenreREC(currYear->firstGenre, genres[i]);                                               //Se agregan los generos a la lista de generos correspondiente.
         
     }
-    else if((strcmp(type, "tvSeries"))==0){
-        currYear->firstSeries = addContentREC(currYear->firstSeries, title, rating, votes, &flag);
-        currYear->seriesCount += flag;
+    else if((strcmp(type, "tvSeries"))==0){                                                                                     //Si es serie
+        currYear->firstSeries = addContentREC(currYear->firstSeries, title, rating, votes, &flag);                              //Se agrega el contenido en la lista de serie
+        currYear->seriesCount += flag;                                                                                          //Si agrego aumenta el contador
     }
     return;
 }
 
-tGenre * addGenreREC(tGenre * first, char * genre){
+static tGenre * addGenreREC(tGenre * first, char * genre){                     //Funcion recursiva para agregar generos en orden alfabetico
     int c;
-    if(first == NULL || (c=strcmp(first->genre, genre)) > 0){
-        tGenre * new = calloc(1, sizeof(tGenre));
+    if(first == NULL || (c=strcmp(first->genre, genre)) > 0){           //Si el genero que agrego es menor al actual o es null la lista
+        tGenre * new = calloc(1, sizeof(tGenre));                       //Agrego el nuevo nodo con el genero.
         if(new == NULL)
             exit(1);
         new->genre = malloc(strlen(genre)+1);
@@ -97,17 +97,18 @@ tGenre * addGenreREC(tGenre * first, char * genre){
         new->tail = first;
         return new;
     }
-    else if(c<0)
+    else if(c<0)                                                        //Genero que agrego mayor al actual, avanzo en la lista.
         first->tail = addGenreREC(first->tail, genre);
-    else                                                              //En este punto c=0;
-        first->count++;
+    else                                                                //En este punto c=0;
+        first->count++;                                                 //Son iguales entonces aumenta el contador
     return first;
 }
 
-static tContent * addContentREC(tContent * first, char * title, float rating, unsigned int votes, int * flag ){
-    if(first == NULL || (first->numVotes < votes) ){
-        tContent * new = calloc(1, sizeof(tContent));
-        if (new == NULL)
+static tContent * addContentREC(tContent * first, char * title, float rating, unsigned int votes, int * flag ){ 
+                                                                        //Recursiva clasica en orden de votos (mayor a menor)
+    if(first == NULL || (first->numVotes < votes) ){                    //Si la lista el NULL o los votos son mayores a la acutal
+        tContent * new = calloc(1, sizeof(tContent));                   //Agregamos el nuevo nodo correspondinte
+        if (new == NULL) 
             exit(1);
         new->title = malloc(strlen(title)+1);
         strcpy(new->title, title);
@@ -118,30 +119,30 @@ static tContent * addContentREC(tContent * first, char * title, float rating, un
         return new;
     }
     else
-        first->tail = addContentREC(first->tail, title, rating, votes, flag);
+        first->tail = addContentREC(first->tail, title, rating, votes, flag);       //Si tengo menos o igual votos avanzo en la lista
     return first;
 }
 
 movieSeriesADT newMovieSeries () {
-    return calloc(1,sizeof(movieSeriesCDT));
+    return calloc(1,sizeof(movieSeriesCDT));                                         //Crea un nuevo adt vacio.
 }
 
 //Funciones para query1
-int currYear(movieSeriesADT movieSeries) {
+int currYear(movieSeriesADT movieSeries) {                                          //Devuelve el anio actual en el cual esta el iterador
     return movieSeries->currYear->year;
 }
 
-int currYearMovieCount(movieSeriesADT movieSeries) {
+int currYearMovieCount(movieSeriesADT movieSeries) {                                //Devuelve el contador de peliculas en el anio del iterador
     return movieSeries->currYear->movieCount;
 
 }
 
-int currYearSeriesCount(movieSeriesADT movieSeries) {
+int currYearSeriesCount(movieSeriesADT movieSeries) {                               //Devuelve el contador de series en el anio del iterador
     return movieSeries->currYear->seriesCount;
 }
 
 //Funciones para query2
-void currGenre(movieSeriesADT movieSeries, char ** genre) {
+void currGenre(movieSeriesADT movieSeries, char ** genre) {                         //Devuelve en un parametro de entrada/salida el string del genero actual en el iterador de genero
     *genre = malloc(strlen(movieSeries->currYear->currGenre->genre) + 1);
     if (*genre == NULL)
         exit(1);
@@ -149,14 +150,14 @@ void currGenre(movieSeriesADT movieSeries, char ** genre) {
 }
 
 int currGenreCount(movieSeriesADT movieSeries) {
-    return movieSeries->currYear->currGenre->count;
+    return movieSeries->currYear->currGenre->count;                                 //Devuelve el contador actual del iterador de genero
 }
 
 //Funciones para query3
 //teniendo en cuenta que las movies y series estan ordenadas de mas votos a menos
-int mostVotedMovie(movieSeriesADT movieSeries, char ** name, int * votes, float * rating) {
-    if(movieSeries->currYear->firstMovie == NULL)
-        return 0;
+int mostVotedMovie(movieSeriesADT movieSeries, char ** name, int * votes, float * rating) {     //Devuelve en varios parametros de entrada/salida
+    if(movieSeries->currYear->firstMovie == NULL)                                               //Toda la data de la pelicula mas votada del iterador year
+        return 0;                                                                               //Si no hay peliculas devuelve 0;
     char * title = malloc(strlen(movieSeries->currYear->firstMovie->title)+1);
     if (title == NULL)
         exit(1);
@@ -164,10 +165,10 @@ int mostVotedMovie(movieSeriesADT movieSeries, char ** name, int * votes, float 
     *name = title;
     *votes = movieSeries->currYear->firstMovie->numVotes;
     *rating = movieSeries->currYear->firstMovie->rating;
-    return 1;
+    return 1;                                                                                    //Si hay pelicular devuelve 1 y toda la data en los parametros
 }
 
-int mostVotedSeries(movieSeriesADT movieSeries, char ** name, int * votes, float * rating) {
+int mostVotedSeries(movieSeriesADT movieSeries, char ** name, int * votes, float * rating) {    //Lo mismo que mostVotedMovie pero con las series
     if(movieSeries->currYear->firstSeries == NULL)
         return 0;
     char * title = malloc(strlen(movieSeries->currYear->firstSeries->title)+1);
@@ -182,59 +183,59 @@ int mostVotedSeries(movieSeriesADT movieSeries, char ** name, int * votes, float
 
 //Controladores del iterador de year
 void toBeginYear(movieSeriesADT movieSeries){
-    movieSeries->currYear = movieSeries->firstYear;
+    movieSeries->currYear = movieSeries->firstYear;                                     //Setea el iterador en el primer anio
 }
 
 int hasNextYear(movieSeriesADT movieSeries){
-    return movieSeries->currYear != NULL;
+    return movieSeries->currYear != NULL;                                               //Se fija si hay un anio valido en el iterador
 }
 
 void nextYear(movieSeriesADT movieSeries){
-    movieSeries->currYear = movieSeries->currYear->tail;
+    movieSeries->currYear = movieSeries->currYear->tail;                                //Cambia el iterador al siguiente anio
 }
 
 //Controladores del iterador de genre
-void toBeginGenre(movieSeriesADT movieSeries) {
-    movieSeries->currYear->currGenre = movieSeries->currYear->firstGenre;
+void toBeginGenre(movieSeriesADT movieSeries) {                                         //Setea el iterador de genero en el primero del iterador de anio
+    movieSeries->currYear->currGenre = movieSeries->currYear->firstGenre;               
 }
 
-int hasNextGenre(movieSeriesADT movieSeries) {
+int hasNextGenre(movieSeriesADT movieSeries) {                                          //Se fija si hay un genero valido en el iterador
     return movieSeries->currYear->currGenre != NULL;
 }
 
-void nextGenre(movieSeriesADT movieSeries) {
+void nextGenre(movieSeriesADT movieSeries) {                                            //Cambia el iterador al siguiente genero
     movieSeries->currYear->currGenre = movieSeries->currYear->currGenre->tail;
 }
 
 
-static void freeContentRec (tContent * list) {
-    if (list == NULL)
+static void freeContentRec (tContent * list) {                                          //Libera todo los contenidos recursivamente
+    if (list == NULL)                                                                   //Clasico free recursivo
         return;
     freeContentRec(list->tail);
     free(list->title);
     free(list);
 }
 
-static void freeGenreRec(tGenre * list) {
-    if (list == NULL)
+static void freeGenreRec(tGenre * list) {                                               //Libera los generos recurivamente
+    if (list == NULL)                                                                   //Clasico free recursivo
         return;
     freeGenreRec(list->tail);
     free(list->genre);
     free(list);
 }
 
-static void freeYearsRec(tYear * yearList) {
+static void freeYearsRec(tYear * yearList) {                                            //Libera todos los anios con sus listas asociadas
     if (yearList == NULL)
         return;
     freeYearsRec(yearList->tail);
-    freeContentRec(yearList->firstMovie);
-    freeContentRec(yearList->firstSeries);
-    freeGenreRec(yearList->firstGenre);
+    freeContentRec(yearList->firstMovie);                                               //Libera las peliculas
+    freeContentRec(yearList->firstSeries);                                              //Libera las series
+    freeGenreRec(yearList->firstGenre);                                                 //Libera los generos
     free(yearList);
 }
 
-void freeMovieSeries(movieSeriesADT movieSeries) {
-    freeYearsRec(movieSeries->firstYear);
-    free(movieSeries);
+void freeMovieSeries(movieSeriesADT movieSeries) {                                      //Libera todo un adt
+    freeYearsRec(movieSeries->firstYear);                                               //Libera todos los anios
+    free(movieSeries);                                                                  
 }
 
