47ef2725357b07eeb7e33aade65fd104e196a1f0
Fermin Bereciartua <ferbereciartua@gmail.com>
2021-07-09 10:53:08 -0300

    errorFlags

--- movieSeriesADT.h
+++ movieSeriesADT.h
@@ -8,46 +8,46 @@ typedef struct movieSeriesCDT * movieSeriesADT;
 movieSeriesADT newMovieSeries ();
 
 //FUNCIONES PARA EL MANEJO DE DATOS
-void addContent(movieSeriesADT adt, int year, char * type, char * title, float rating, unsigned int votes, char ** genres );
+void addContent (movieSeriesADT adt, int year, char * type, char * title, float rating, unsigned int votes, char ** genres, int * errorFlag);
 
 //FUNCIONES PARA QUERY1
 //devuelve el anio actual en el que esta el iterador
-int currYear(movieSeriesADT movieSeries);
+int currYear (movieSeriesADT movieSeries);
 //devuelve el numero de movies del anio actual
-int currYearMovieCount(movieSeriesADT movieSeries);
+int currYearMovieCount (movieSeriesADT movieSeries);
 //devuelve el numero de series del anio actual
-int currYearSeriesCount(movieSeriesADT movieSeries);
+int currYearSeriesCount (movieSeriesADT movieSeries);
 
 //FUNCIONES PARA QUERY2
 //devuelve una copia del nombre del genre actual
-void currGenre(movieSeriesADT movieSeries, char ** genre);
+void currGenre (movieSeriesADT movieSeries, char ** genre);
 //devuelve la cantidad de genres en el anio actual
-int currGenreCount(movieSeriesADT movieSeries);
+int currGenreCount (movieSeriesADT movieSeries);
 
 //FUNCIONES PARA QUERY3
 //devuelve un puntero al tContent con mas votos
 //devuelve por parametros de salida el nombre, la cantidad de votos y el rating
 //de la serie mas votada del currYear
-int mostVotedMovie(movieSeriesADT movieSeries, char ** name, int * votes, float * rating);
+int mostVotedMovie (movieSeriesADT movieSeries, char ** name, int * votes, float * rating, int * errorFlag);
 //devuelve por parametros de salida el nombre, la cantidad de votos y el rating
 //de la serie mas votada del currYear
-int mostVotedSeries(movieSeriesADT movieSeries, char ** name, int * votes, float * rating);
+int mostVotedSeries (movieSeriesADT movieSeries, char ** name, int * votes, float * rating, int * errorFlag);
 
 
 //void free(movieSeriesADT movieSeries);
-void freeMovieSeries(movieSeriesADT movieSeries);
+void freeMovieSeries (movieSeriesADT movieSeries);
 //Controladores del iterador de year
-void toBeginYear(movieSeriesADT movieSeries);
+void toBeginYear (movieSeriesADT movieSeries);
 
-int hasNextYear(movieSeriesADT movieSeries);
+int hasNextYear (movieSeriesADT movieSeries);
 
-void nextYear(movieSeriesADT movieSeries);
+void nextYear (movieSeriesADT movieSeries);
 
 //Controladores del iterador de genre
-void toBeginGenre(movieSeriesADT movieSeries);
+void toBeginGenre (movieSeriesADT movieSeries);
 
-int hasNextGenre(movieSeriesADT movieSeries);
+int hasNextGenre (movieSeriesADT movieSeries);
 
-void nextGenre(movieSeriesADT movieSeries);
+void nextGenre (movieSeriesADT movieSeries);
 
 #endif
